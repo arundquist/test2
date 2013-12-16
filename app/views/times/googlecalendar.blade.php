2926c0d73f69782833ca035d509405579cd7cba7
Andy Rundquist <andy.rundquist@gmail.com>
2013-12-15 21:27:49 -0600

    got rid of the br's
    pesky brs!

--- app/views/times/googlecalendar.blade.php
+++ app/views/times/googlecalendar.blade.php
@@ -1,20 +1,19 @@
-BEGIN:VCALENDAR<br/>
+BEGIN:VCALENDAR
 @foreach ($courses AS $course)
-<br/>
+
 @foreach($course->times AS $time)
 <?php $var=strtoupper($time->day);
 eval("\$dt= Carbon\Carbon::now()->next(Carbon\Carbon::$var);");
 $newdt=Carbon\Carbon::createFromFormat('Y-m-j h:ia', "{$dt->toDateString()} {$time->beginning}");
 $newdt2=Carbon\Carbon::createFromFormat('Y-m-j h:ia', "{$dt->toDateString()} {$time->end}");
 ?>
-BEGIN:VEVENT<br/>
-DTSTART:{{$newdt->format('Ymd\THis') }}<br/>
-DTEND:{{$newdt2->format('Ymd\THis') }}<br/>
-DESCRIPTION:<br/>
-LOCATION:{{$course->room->building->name}} {{$course->room->number}}<br/>
-SUMMARY:{{$course->title}}<br/>
-END:VEVENT<br/>
-<br/>
+BEGIN:VEVENT
+DTSTART:{{$newdt->format('Ymd\THis') }}
+DTEND:{{$newdt2->format('Ymd\THis') }}
+DESCRIPTION:
+LOCATION:{{$course->room->building->name}} {{$course->room->number}}
+SUMMARY:{{$course->title}}
+END:VEVENT
 
 
 @endforeach
