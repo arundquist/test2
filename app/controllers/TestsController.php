1dfbacca130fc69270f0e9f9ba51694ea540ede1
Andy Rundquist <andy.rundquist@gmail.com>
2014-08-28 11:54:43 -0500

    fixed tests

--- app/controllers/TestsController.php
+++ app/controllers/TestsController.php
@@ -5,9 +5,7 @@ class TestsController extends \BaseController {
 	public function getUpdated($term_id)
 	{
 		$term=Term::findOrFail($term_id);
-		$courses=$term->courses()
-			->where('updated_at','<', date('Y-m-d'))
-			->get();
+		$courses=$term->courses;
 		dd(count($courses->lists('updated_at')));
 	}
 
