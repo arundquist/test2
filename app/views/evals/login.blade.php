6d681f53e42596511beef5cad9df7114845b9210
Andy Rundquist <andy.rundquist@gmail.com>
2015-01-22 11:11:18 -0600

    change login page

--- app/views/evals/login.blade.php
+++ app/views/evals/login.blade.php
@@ -4,15 +4,15 @@
 <div>
 Welcome. This is an experimental interface to Hamline's student evaluation
 data. You put in your piperline login information, and the script logs you in and then
-retrieves the evaluation data for this course (whose course "id" is the last
-part of the url for this page). If you're not allowed access to that
+gives you a list of faculty to choose from along with which terms you want.
+If you're not allowed access to that
 course's evaluation data (which is true for all students and nearly all faculty) this
 page should fail with all kinds of weird warnings. If you do have access (ie you're
 the teacher of the course or you're a chair of the teacher etc) you should
 see a formatted table of scores. Have fun!
 </div>
 {{Form::open(['method'=>'post', 'action'=>['EvalsController@postLogin']])}}
-{{Form::text('username', null, ['placeholder'=>'username'])}}<br/>
+{{Form::text('username', null, ['placeholder'=>'Hamline id'])}}<br/>
 {{Form::password('password', null, ['placeholder'=>'password'])}}<br/>
 {{Form::submit('submit')}}<br/>
 {{Form::close()}}
