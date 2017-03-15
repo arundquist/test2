394e7471296e5d774ec6b8687e1ffa5f387cf951
Andy Rundquist <andy.rundquist@gmail.com>
2017-03-15 13:50:38 -0500

    show raw data on enrollment plot

--- app/views/history/enrollment.blade.php
+++ app/views/history/enrollment.blade.php
@@ -20,10 +20,27 @@
         chart.draw(data, options);
       }
     </script>
- 
+
   <h1>{{$courses[0]->title}}</h1>
     <div id="chart_div" style="width: 900px; height: 500px;"></div>
-    
-    
+
+
     {{$courses[0]->description}}
+
+    <div>
+      <table class='table'>
+        <tr>
+          <th>Term</th>
+          <th>enrollment</th>
+          <th>cap</th>
+        </tr>
+        @foreach ($courses as $course)
+          <tr>
+            <td>{{$course->term->ay}} {{$course->term->season}} {{$course->section}}</td>
+            <td>{{$course->enrollment}}</td>
+            <td>{{$course->enrollmentmax}}</td>
+          </tr>
+        @endforeach
+      </table>
+    </div>
 @stop
