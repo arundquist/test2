48f44a0413bc4488c9dea12414bdce405d25bdad
Andy Rundquist <andy.rundquist@gmail.com>
2017-10-17 13:49:48 -0500

    add fall 2018 to bookmarklets

--- app/views/tests/bookmarklets.blade.php
+++ app/views/tests/bookmarklets.blade.php
@@ -25,5 +25,27 @@ document.querySelector(&#39;[title="End Date"]&#39;).innerHTML="Thursday May 10,
 })();
 '>TR S18</a> for standard TR classes in spring 2018 on <a href='https://www.hamline.edu/Content.aspx?ekfrm=4294977374'>this new course form</a>
   </li>
+  <li class="list-group-item">
+    <a href='javascript:(function(){
+document.getElementById("StartDate").value = "09/05/2018";
+document.getElementById("StartDate_iso").value="2018-09-05";
+document.querySelector(&#39;[title="Start Date"]&#39;).innerHTML="Wednesday September 5, 2018";
+document.getElementById("EndDate").value = "12/14/2018";
+document.getElementById("EndDate_iso").value="2018-12-14";
+document.querySelector(&#39;[title="End Date"]&#39;).innerHTML="Friday December 14, 2018";
+})();
+'>MWF F18</a> for standard MWF classes in fall 2018 on <a href='https://www.hamline.edu/Content.aspx?ekfrm=4294977374'>this new course form</a>
+  </li>
+  <li class="list-group-item">
+    <a href='javascript:(function(){
+document.getElementById("StartDate").value = "09/06/2018";
+document.getElementById("StartDate_iso").value="2018-09-06";
+document.querySelector(&#39;[title="Start Date"]&#39;).innerHTML="Thursday September 6, 2018";
+document.getElementById("EndDate").value = "12/13/2018";
+document.getElementById("EndDate_iso").value="2018-12-13";
+document.querySelector(&#39;[title="End Date"]&#39;).innerHTML="Thursday December 13, 2018";
+})();
+'>MWF F18</a> for standard MWF classes in fall 2018 on <a href='https://www.hamline.edu/Content.aspx?ekfrm=4294977374'>this new course form</a>
+  </li>
 </ul>
 @stop
